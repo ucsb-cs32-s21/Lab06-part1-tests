662c4b573324c29882ea715015bb6c012c1f48d3
Zoe Wood <zwood@calpoly.edu>
2021-05-16 16:11:17 -0700

    Add files via upload

---
+++ visitor.h
@@ -0,0 +1,13 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+
+#include "psData.h"
+#include "psCombo.h"
+#include "demogData.h"
+
+/* a visitor can visit any of my specific data types */
+class Visitor {
+  //FILL IN
+};
+#endif
+
---
+++ visitorReport.h
@@ -0,0 +1,13 @@
+#ifndef VISITREP_H
+#define VISITREP_H
+
+#include "psData.h"
+#include "demogData.h"
+#include "visitor.h"
+#include <iomanip>
+
+class visitorReport : public Visitor {
+//FILL IN
+};
+
+#endif
